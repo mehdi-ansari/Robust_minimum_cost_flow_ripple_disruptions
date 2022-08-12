4684bb4d37af1672ebd04189b19342d085767bdd
mehdi-ansari <59858967+mehdi-ansari@users.noreply.github.com>
2022-08-12 13:53:03 -0500

    Add files via upload

---
+++ Revision/LinearModel_BR/GeneratingCuts.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+vector<double> MYaddingC(int m0, vector<int> k0, int NumberOfAttacks0, vector<double> vals0,
+	vector<double> Yoptimal0, vector<double> p1a0, vector<double> p2a0, vector<double> Alp10,
+	vector<double> Alp20, vector<vector<double> > d0, vector<vector<double> > q0, vector<double> C0a0);
---
+++ Revision/LinearModel_BR/InitialCosts.h
@@ -0,0 +1,14 @@
+#include <ilcplex/ilocplex.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ctime>    // For time()
+#include <cstdlib>  // For srand() and rand()
+#include <vector>
+#include <math.h>       /* fmod */
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+vector <vector <double> > InitialCosts(int n0, int m0, int Ly, int H0, vector <double> x0, vector<vector <double> > y0);
